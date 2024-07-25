975d6708f39fb61b42ce751f2779982c59819599
Vagarth <vagarth419@gmail.com>
2024-07-25 13:40:40 +0530

    feat: add left sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,16 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = {firstName: 'Vagarth', lastName: 'Pandey'};
+
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
         {children}
     </main>
   );
