1bd6cafed282b69d191f4ed369f6d50a357a6419
Vagarth <vagarth419@gmail.com>
2024-07-27 18:43:41 +0530

    feat: add right siderbar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
+import RightSidebar from '@/components/RightSidebar';
 
 const Home = () => {
 
-    const loggedIn = {firstName: 'Vagarth'};
+    const loggedIn = {firstName: 'Vagarth', lastName: 'Pandey', email: 'vagarth419@gmail.com'};
 
   return (
     <section className='home'>
@@ -13,7 +14,9 @@ const Home = () => {
                 <HeaderBox type="greeting" title="Welcome" user={loggedIn?.firstName || 'Guest'} subtext="Manage your expenses" />
                 <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1720} />
             </header>
+            {/* recent transactions */}
         </div>
+        <RightSidebar user={loggedIn} tranbsactions={[]} banks={[{currentBalance: 120.01}, {currentBalance: 500.00}]} />
     </section>
   )
 }
